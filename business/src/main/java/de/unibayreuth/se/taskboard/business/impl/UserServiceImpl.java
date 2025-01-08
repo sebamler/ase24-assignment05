a6f83af0cdc6acf72d80d0f5cd008bc5fac1cf59
sebamler <sebastian.amler@uni-bayreuth.de>
2025-01-08 17:51:07 +0100

    cleared the first 2 TODOs in UserService.java by also creating UserServiceImpl.java

---
+++ business/src/main/java/de/unibayreuth/se/taskboard/business/impl/UserServiceImpl.java
@@ -0,0 +1,43 @@
+package de.unibayreuth.se.taskboard.business.impl;
+
+import de.unibayreuth.se.taskboard.business.domain.User;
+import de.unibayreuth.se.taskboard.business.exceptions.DuplicateNameException;
+import de.unibayreuth.se.taskboard.business.exceptions.MalformedRequestException;
+import de.unibayreuth.se.taskboard.business.exceptions.UserNotFoundException;
+import de.unibayreuth.se.taskboard.business.ports.UserPersistenceService;
+import de.unibayreuth.se.taskboard.business.ports.UserService;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+
+    private final UserPersistenceService userPersistenceService;
+
+    @Override
+    @NonNull
+    public List<User> getAllUsers() {
+        return userPersistenceService.getAll();
+    }
+
+    @Override
+    @NonNull
+    public User getById(@NonNull UUID id) throws UserNotFoundException {
+        return userPersistenceService.getById(id)
+                .orElseThrow(() -> new UserNotFoundException("User with ID " + id + " does not exist."));
+    }
+
+    @Override
+    @NonNull
+    public User create(@NonNull User user) throws MalformedRequestException, DuplicateNameException {
+        if (user.getId() != null) {
+            throw new MalformedRequestException("User id must not be set");
+        }
+        return userPersistenceService.upsert(user);
+    }
+}
--- business/src/main/java/de/unibayreuth/se/taskboard/business/ports/UserService.java
+++ business/src/main/java/de/unibayreuth/se/taskboard/business/ports/UserService.java
@@ -1,6 +1,23 @@
 package de.unibayreuth.se.taskboard.business.ports;
 
+import de.unibayreuth.se.taskboard.business.domain.User;
+import de.unibayreuth.se.taskboard.business.exceptions.MalformedRequestException;
+import de.unibayreuth.se.taskboard.business.exceptions.UserNotFoundException;
+import org.springframework.lang.NonNull;
+
+import java.util.List;
+import java.util.UUID;
+
 public interface UserService {
-    //TODO: Add user service interface that the controller uses to interact with the business layer.
-    //TODO: Implement the user service interface in the business layer, using the existing user persistence service.
+    //DONE: Add user service interface that the controller uses to interact with the business layer.
+    @NonNull
+    List<User> getAllUsers();
+
+    @NonNull
+    User getById(@NonNull UUID id) throws UserNotFoundException;
+
+    @NonNull
+    User create(@NonNull User user) throws MalformedRequestException;
+
+    //DONE: Implement the user service interface in the business layer, using the existing user persistence service.
 }
